ab176d4a5b57051aebd312dcb37b23e5d9dcaf8b
Tracie <traciethornbury@gmail.com>
2022-04-18 21:15:16 -0700

    lab 4
    Donezo

---
+++ lab4/js/lab.js
@@ -0,0 +1,35 @@
+// Lab 4: Pseudocoding and Problem-solving.
+// Psuedo code for rock paper scissors.
+// @link   URL
+// @lab4/js/lab.js
+// @author Tracie Thornbury.
+// @since  4/18/2022
+
+
+/***
+
+Print the options for the game
+  1 - Rock
+  2 - Paper
+  3 - Scissors
+
+Ask users input for which one they would like to pick
+  Verify that the input is correct, otherwise loop back to 2
+Bot randomly picks a number between 1-3
+  1 - rock
+  2 - paper
+  3 - scissors
+Print the winner is based on who won with these parameters
+  Rock
+    Rock beats paper
+  Paper
+    Paper beats rock
+  Scissors
+    Scissors beats paper
+  Draw - both input value are the same
+    Loop back to 2
+Ask the user if they want to replay
+  If yes loop back to 1
+  If no end the game
+
+***/

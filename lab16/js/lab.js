232363a1f6c9a147225d772a816222825a46c406
Tracie <traciethornbury@gmail.com>
2022-06-03 04:26:27 -0700

    wa

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -7,7 +7,7 @@
 
  // Using the core $.ajax() method
  $.ajax({
-   url: "http://xkcd.com/info.0.json",
+   url: "https://xkcd.com/info.0.json",
    type: "GET",
    crossDomain: true,
    dataType: "json",

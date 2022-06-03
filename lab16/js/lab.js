142d49a1be8ffd386a9ef308690d15abed5c2cb2
Tracie <traciethornbury@gmail.com>
2022-06-03 04:41:05 -0700

    e

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -8,7 +8,7 @@ console.log("im dying");
 
  // Using the core $.ajax() method
  $.ajax({
-   url: "https://xkcd.com/614/info.0.json",
+   url: "https://xkcd.com/info.0.json",
    type: "GET",
    crossDomain: true,
    dataType: "json",
